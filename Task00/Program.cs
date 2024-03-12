2b43b76b3da0f2985c0dfc1893fe5aa7767d6305
Ilya <ilyamonya@gmail.com>
2024-03-12 23:43:13 +0900

    add some

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -5,5 +5,12 @@
     4 -> 16
     -3 -> 9
     -7 -> 49
-    
+
 */
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number * number;
+Console.WriteLine(result);
