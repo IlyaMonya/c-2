8df9349d33c9109b045eac224d540b2954ddfe27
Ilya <ilyamonya@gmail.com>
2024-03-13 02:06:48 +0900

    решили задачу 7

---
+++ Task07/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Напишите программу которая принимает на вход трезначное число и на выходе показывает последнюю цифру этого числа
+*/
+
+Console.WriteLine("напишите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number%10);
+
